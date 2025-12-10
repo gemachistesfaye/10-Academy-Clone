b5ddd873ba48926e31eafc50309ce30220aaf9fe
https://github.com/urjiiko1/code-craft/commits?author=urjiiko1 <gemachistesfaye36@gmail.com>
2025-12-11 00:33:12 +0300

    updated the 10 academy clone webiste

--- Script/Home.js
+++ Script/Home.js
@@ -22,3 +22,50 @@
     }
   });
 
+
+const cards = document.querySelectorAll(".alumni-card");
+
+let index = 0;
+
+// random positions on map
+const positions = [
+  { left: "50%", top: "50%" },
+  { left: "25%", top: "40%" },
+  { left: "70%", top: "45%" },
+  { left: "60%", top: "75%" },
+  { left: "40%", top: "70%" },
+  { left: "80%", top: "60%" }
+];
+
+function showCard(i) {
+  cards.forEach(card => card.classList.remove("active"));
+
+  const card = cards[i];
+  const pos = positions[Math.floor(Math.random() * positions.length)];
+
+  card.style.left = pos.left;
+  card.style.top = pos.top;
+
+  card.classList.add("active");
+}
+
+showCard(index);
+setInterval(() => {
+  index = (index + 1) % cards.length;
+  showCard(index);
+}, 4000);
+
+
+const items = document.querySelectorAll(".accordion-item");
+
+items.forEach(item => {
+  item.querySelector(".accordion-header").addEventListener("click", () => {
+
+    // Close all items instantly
+    items.forEach(i => i.classList.remove("active"));
+
+    // Open only clicked item
+    item.classList.add("active");
+  });
+});
+
